f27d580a9be728442d482a5ff76776f4b5d4c546
ErisianArchitect <derekforgione@gmail.com>
2025-01-31 05:06:56 -0800

    Documentation and debug_panic/debug_panic_if.

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,85 @@
-use std::fmt::Display;
+//! This crate provides reusable error messages ([PanicMsg]) for use with 
+//! panics, assertions (`assert`, `assert_eq`, `assert_ne`), and `expect`.
+//! It also includes debug versions of each of these methods.
+//! 
+//! Simply declare a [PanicMsg] in your code:
+//! ```rust, no_run
+//! const EXAMPLE_PANIC: PanicMsg = PanicMsg::new("This is an example panic message.");
+//! ```
+//! Then you can use it like this:
+//! ```rust, no_run
+// ...
+//! EXAMPLE_PANIC.panic();
+//! // ...
+//! EXAMPLE_PANIC.panic_if(left >= right);
+//! // ...
+//! EXAMPLE_PANIC.assert(left < right);
+//! // ...
+//! EXAMPLE_PANIC.assert_eq(left, right);
+//! // ...
+//! EXAMPLE_PANIC.assert_ne(left, right);
+//! // ...
+//! EXAMPLE_PANIC.expect_opt(option_value);
+//! // ...
+//! EXAMPLE_PANIC.expect_result(result);
+//! // ...
+//! EXAMPLE_PANIC.debug_panic();
+//! // ...
+//! EXAMPLE_PANIC.debug_panic_if(left >= right);
+//! // ...
+//! EXAMPLE_PANIC.debug_assert(left < right);
+//! // ...
+//! EXAMPLE_PANIC.debug_assert_eq(left, right);
+//! // ...
+//! EXAMPLE_PANIC.debug_assert_ne(left, right);
+//! // ...
+//! EXAMPLE_PANIC.debug_expect_opt(option_value);
+//! // ...
+//! EXAMPLE_PANIC.debug_expect_result(result);
+//! ```
 
-/// A panic message that can be initialized at compile time and used for panics with
-/// specific error messages that are reuseable.
-pub struct PanicMsg<M: Display = &'static str> {
+/// A compile-time initialized panic message that can be reused with specific error messages
+/// for panics, allowing for consistent and reusable error reporting.
+/// 
+/// # Example
+/// ```rust, no_run
+/// const EXAMPLE_PANIC: PanicMsg = PanicMsg::new("This is an example panic message.");
+/// // ...
+/// EXAMPLE_PANIC.panic();
+/// // ...
+/// EXAMPLE_PANIC.panic_if(left >= right);
+/// // ...
+/// EXAMPLE_PANIC.assert(left < right);
+/// // ...
+/// EXAMPLE_PANIC.assert_eq(left, right);
+/// // ...
+/// EXAMPLE_PANIC.assert_ne(left, right);
+/// // ...
+/// EXAMPLE_PANIC.expect_opt(option_value);
+/// // ...
+/// EXAMPLE_PANIC.expect_result(result);
+/// ```
+/// There are also `debug` variants of each of these methods.
+pub struct PanicMsg<M: std::fmt::Display = &'static str> {
     message: M,
 }
 
-impl<M: Display> PanicMsg<M> {
+impl<M: std::fmt::Display> PanicMsg<M> {
     pub const fn new(message: M) -> Self {
         Self { message }
     }
 
-    /// Panic with inner message.
+    /// Panic at runtime.
+    /// 
+    /// See [panic].
     #[track_caller]
     pub fn panic(&self) -> ! {
         panic!("{}", self.message);
     }
 
-    #[track_caller]
-    pub fn debug_panic(&self) {
-        if cfg!(debug_assertions) {
-            panic!("{}", self.message);
-        }
-    }
-
-    /// Panic if the condition is met.
+    /// Panic if the condition is `true` at runtime.
+    /// 
+    /// See [panic].
     #[track_caller]
     pub fn panic_if(&self, condition: bool) {
         if condition {
@@ -32,21 +87,17 @@ impl<M: Display> PanicMsg<M> {
         }
     }
 
-    #[track_caller]
-    pub fn debug_panic_if(&self, condition: bool) {
-        #[cfg(debug_assertions)]
-        if condition {
-            panic!("{}", self.message);
-        }
-    }
-
-    /// Assert the condition.
+    /// Asserts that a boolean expression is `true` at runtime.
+    /// 
+    /// see [assert].
     #[track_caller]
     pub fn assert(&self, condition: bool) {
         assert!(condition, "{}", self.message);
     }
 
-    /// Assert if two expressions are equal.
+    /// Assert that two expressions are equal to each other (using [PartialEq]).
+    /// 
+    /// See [assert_eq].
     #[track_caller]
     pub fn assert_eq<L, R>(&self, lhs: L, rhs: R)
     where
@@ -57,7 +108,9 @@ impl<M: Display> PanicMsg<M> {
         assert_eq!(lhs, rhs, "{}", self.message);
     }
 
-    /// Assert if two expressions are not equal.
+    /// Assert that two expressions are not equal to each other (using [PartialEq]).
+    /// 
+    /// See [assert_ne].
     #[track_caller]
     pub fn assert_ne<L, R>(&self, lhs: L, rhs: R)
     where
@@ -68,13 +121,39 @@ impl<M: Display> PanicMsg<M> {
         assert_ne!(lhs, rhs, "{}", self.message);
     }
 
-    /// Assert condition in debug mode.
+    /// Panic at runtime with `debug_assertions`.
+    /// 
+    /// See [panic].
+    #[track_caller]
+    pub fn debug_panic(&self) {
+        if cfg!(debug_assertions) {
+            panic!("{}", self.message);
+        }
+    }
+
+    /// Panic if the condition is `true` at runtime with `debug_assertions`.
+    /// 
+    /// See [panic].
+    #[track_caller]
+    pub fn debug_panic_if(&self, condition: bool) {
+        #[cfg(debug_assertions)]
+        if condition {
+            panic!("{}", self.message);
+        }
+    }
+
+    /// Asserts that a boolean expression is `true` at runtime with `debug_assertions`.
+    /// 
+    /// see [assert].
     #[track_caller]
     pub fn debug_assert(&self, condition: bool) {
         debug_assert!(condition, "{}", self.message);
     }
 
-    /// Assert two expressions are equal in debug mode.
+    /// Assert that two expressions are equal to each other (using [PartialEq])
+    /// with `debug_assertions`.
+    /// 
+    /// See [assert_eq].
     #[track_caller]
     pub fn debug_assert_eq<L, R>(&self, lhs: L, rhs: R)
     where
@@ -85,7 +164,10 @@ impl<M: Display> PanicMsg<M> {
         debug_assert_eq!(lhs, rhs, "{}", self.message);
     }
 
-    /// Assert two expressions are not equal in debug mode.
+    /// Assert that two expressions are not equal to each other (using [PartialEq])
+    /// with `debug_assertions`.
+    /// 
+    /// See [assert_ne].
     #[track_caller]
     pub fn debug_assert_ne<L, R>(&self, lhs: L, rhs: R)
     where
@@ -96,7 +178,7 @@ impl<M: Display> PanicMsg<M> {
         debug_assert_ne!(lhs, rhs, "{}", self.message);
     }
 
-    /// Expect an [Option<T>] value.
+    /// Returns the contained [Some] value, consuming the `self` value
     #[track_caller]
     pub fn expect_opt<T>(&self, value: Option<T>) -> T {
         let Some(value) = value else {
@@ -105,7 +187,7 @@ impl<M: Display> PanicMsg<M> {
         value
     }
 
-    /// Expect a [Result<T, E>] value.
+    /// Returns the contained [Ok] value, consuming the `self` value.
     #[track_caller]
     pub fn expect_result<T, E>(&self, value: Result<T, E>) -> T {
         let Ok(value) = value else {
@@ -123,7 +205,7 @@ impl<M: Display> PanicMsg<M> {
 
 // Trait implementations.
 
-impl<M: Display + Clone> Clone for PanicMsg<M> {
+impl<M: std::fmt::Display + Clone> Clone for PanicMsg<M> {
     fn clone(&self) -> Self {
         Self {
             message: self.message.clone()
@@ -135,9 +217,9 @@ impl<M: Display + Clone> Clone for PanicMsg<M> {
     }
 }
 
-impl<M: Display + Copy> Copy for PanicMsg<M> {}
+impl<M: std::fmt::Display + Copy> Copy for PanicMsg<M> {}
 
-impl<M: Display + PartialEq> PartialEq<M> for PanicMsg<M> {
+impl<M: std::fmt::Display + PartialEq> PartialEq<M> for PanicMsg<M> {
     fn eq(&self, other: &M) -> bool {
         self.message == *other
     }
@@ -147,7 +229,7 @@ impl<M: Display + PartialEq> PartialEq<M> for PanicMsg<M> {
     }
 }
 
-impl<M: Display + PartialEq> PartialEq<PanicMsg<M>> for PanicMsg<M> {
+impl<M: std::fmt::Display + PartialEq> PartialEq<PanicMsg<M>> for PanicMsg<M> {
     fn eq(&self, other: &PanicMsg<M>) -> bool {
         self.message == other.message
     }
@@ -157,27 +239,27 @@ impl<M: Display + PartialEq> PartialEq<PanicMsg<M>> for PanicMsg<M> {
     }
 }
 
-impl<M: Display + Eq> Eq for PanicMsg<M> {}
+impl<M: std::fmt::Display + Eq> Eq for PanicMsg<M> {}
 
-impl<M: Display + PartialOrd> PartialOrd<M> for PanicMsg<M> {
+impl<M: std::fmt::Display + PartialOrd> PartialOrd<M> for PanicMsg<M> {
     fn partial_cmp(&self, other: &M) -> Option<std::cmp::Ordering> {
         self.message.partial_cmp(other)
     }
 }
 
-impl<M: Display + PartialOrd> PartialOrd<PanicMsg<M>> for PanicMsg<M> {
+impl<M: std::fmt::Display + PartialOrd> PartialOrd<PanicMsg<M>> for PanicMsg<M> {
     fn partial_cmp(&self, other: &PanicMsg<M>) -> Option<std::cmp::Ordering> {
         self.message.partial_cmp(&other.message)
     }
 }
 
-impl<M: Display + Ord> Ord for PanicMsg<M> {
+impl<M: std::fmt::Display + Ord> Ord for PanicMsg<M> {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
         self.message.cmp(&other.message)
     }
 }
 
-impl<M: Display + std::hash::Hash> std::hash::Hash for PanicMsg<M> {
+impl<M: std::fmt::Display + std::hash::Hash> std::hash::Hash for PanicMsg<M> {
     fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
         self.message.hash(state);
     }
@@ -192,6 +274,17 @@ impl<M: Display + std::hash::Hash> std::hash::Hash for PanicMsg<M> {
     }
 }
 
+/// Declare a const [PanicMsg] with an `&'static str` message.
+/// 
+/// # Example
+/// ```rust, no_run
+/// const_panic_msg!(PRIVATE_ERROR = "This is declared with private visibility.");
+/// const_panic_msg!(pub PUBLIC_ERROR = "This is declared with public visiblity.");
+/// // ...
+/// PRIVATE_ERROR.panic();
+/// // ...
+/// PUBLIC_ERROR.debug_assert(left < right);
+/// ```
 #[macro_export]
 macro_rules! const_panic_msg {
     ($visibility:vis $name:ident = $msg:literal) => {
@@ -208,7 +301,7 @@ mod tests {
     #[should_panic]
     #[test]
     fn panic_test() {
-        
+
         TEST_PANIC.panic();
     }
 }
